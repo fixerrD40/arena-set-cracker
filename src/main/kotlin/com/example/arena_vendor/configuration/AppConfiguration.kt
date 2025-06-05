f91902e2596ff70135bebcfd2d73ad780c3127be
fixerrD40 <stafford.hank@gmail.com>
2025-06-05 14:12:05 +0200

    Avignon
    - Cors in my frontend

--- src/main/kotlin/com/example/arena_vendor/configuration/AppConfiguration.kt
+++ src/main/kotlin/com/example/arena_vendor/configuration/AppConfiguration.kt
@@ -3,10 +3,22 @@ package com.example.arena_vendor.configuration
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration(proxyBeanMethods = false)
 class AppConfiguration {
 
     @Bean
     fun objectMapper() = jacksonObjectMapper()
+
+    @Configuration
+    class WebConfig : WebMvcConfigurer {
+        override fun addCorsMappings(registry: CorsRegistry) {
+            registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "DELETE")
+                .allowedHeaders("*")
+        }
+    }
 }
