bdf33e508a7afbe47f185894e1f9fb25d1c9dd94
fixerrD40 <stafford.hank@gmail.com>
2025-08-19 10:27:13 -0500

    Hideaway
    - Enable feign clients to get build working

--- src/main/kotlin/com/example/arena_set_cracker/ArenaSetCrackerApplication.kt
+++ src/main/kotlin/com/example/arena_set_cracker/ArenaSetCrackerApplication.kt
@@ -2,8 +2,10 @@ package com.example.arena_set_cracker
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableFeignClients
 class ArenaSetCrackerApplication
 
 fun main(args: Array<String>) {
